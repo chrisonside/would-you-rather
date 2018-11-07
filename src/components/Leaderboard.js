6a0dd3754aacce2335643a64771d090df2b75f50
Chris Reidy <christopher.t.reidy@gmail.com>
2018-11-07 09:25:02 +0000

    Add basic leaderboard functionality

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,33 +3,54 @@ import { connect } from 'react-redux';
 import { isObjectEmpty } from '../utils/helper';
 import { Link } from 'react-router-dom';
 
-import SignIn from './SignIn';
-
-// import {
-//   getQuestions
-// } from '../actions';
-
 import { isArrayEmpty } from '../utils/helper';
 
 class Leaderboard extends Component {
 
   render() {
-    const { loggedInUser } = this.props;
+    const { usersArray } = this.props;
 
     return (
       <div className='leaderboard'>
-        <div>
-          Leaderboard goes here
-        </div>
+        <h1>
+          Leaderboard
+        </h1>
+        {!isArrayEmpty(usersArray) && usersArray.map((user, index) => (
+          <div key={index}>
+            <p className='leaderboard__name'>{user.name}</p>
+            <img className='leaderboard__avatar' src={user.avatarURL} alt={user.name} />
+            <p className='leaderboard__answers'>Questions answered: {user.answers}</p>
+            <p className='leaderboard__questions'>Questions asked: {user.questions}</p>
+            <p className='leaderboard__score'>Total score: {user.score}</p>
+          </div>
+        ))}
       </div>
 
     );
   }
 }
 
-function mapStateToProps( {loggedInUser} ) {
+function convertToArray(object) {
+  return Object.keys(object).map(i => object[i]);
+}
+
+function mapStateToProps( {users} ) {
+  // const sortedUsers = prepData(users);
+  let usersArray = [];
+  if(!isObjectEmpty(users)) {
+    usersArray = convertToArray(users);
+    usersArray.forEach(function(item, index){
+      item.questions = item.questions.length;
+      item.answers = Object.keys(item.answers).length;
+      item.score = item.questions + item.answers;
+    });
+    usersArray.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  }
+
   return {
-    loggedInUser
+    usersArray
  }
 }
 
