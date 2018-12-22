5b78548e7a7c1afcd85c445ca16a344a6d6b9c62
Chris Reidy <christopher.t.reidy@gmail.com>
2018-12-22 23:54:29 +0000

    Add Spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+const Spinner = () =>
+  <div>
+    <div className='poll-list__spinner-spacer'></div>
+    <Loader
+      type="Puff"
+      color="#00ADB5"
+      height="100"
+      width="100"
+    />
+    <div className='poll-list__spinner-spacer'></div>
+  </div>
+
+export default Spinner;
