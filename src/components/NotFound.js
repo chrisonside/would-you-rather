ed32be0a1131a8bfbc2cbe2b2d1578f10f444a0f
Chris Reidy <christopher.t.reidy@gmail.com>
2018-12-22 23:56:33 +0000

    Add 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import ConfusedLogo from '../assets/confused.png';
+
 import {
   clearCurrentPoll,
 } from '../actions';
@@ -13,9 +15,10 @@ class NotFound extends Component {
 
   render() {
     return (
-      <div className='notfound'>
-        <h1>404 - page not found</h1>
-        <Link to='/' className='notfound__link' onClick={() => this.clearSelectedPoll()}>Back to polls</Link>
+      <div className='not-found'>
+        <img src={ConfusedLogo} className='not-found__logo' alt='Confused person'/>
+        <h1 className='not-found__title'>404 - page not found</h1>
+        <Link to='/' className='not-found__link' onClick={() => this.clearSelectedPoll()}>Back to polls</Link>
       </div>
     );
   }
